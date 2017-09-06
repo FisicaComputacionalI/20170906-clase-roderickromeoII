8fb788203893f56380b522ad520611e3076c0d63
Rodrigo Romero-de Lazaro <31290126+roderickromeoII@users.noreply.github.com>
2017-09-06 12:58:32 -0500

    Correction

--- graph.py
+++ graph.py
@@ -12,11 +12,11 @@ x1 = [7000,8000,9000,10000,11000]
 # Crea un vector (arreglo) con los valores en el eje Y para cada valor en el eje X
 y1 = [80,83,85,86,88] 
 # Grafica el vector x contra el vector y
-pl.axis ([0,5, 0,10])
+pl.axis ([0,100, 5000,12000])
 pl.plot(x1,y1)
 pl.plot(x1,y1, 'gD')
-pl.ylabel('Voltaje')
-pl.xlabel('Eficiencia')
+pl.ylabel('Voltaje [V]')
+pl.xlabel('Eficiencia [%]')
 #Guarda la grafica en el formato png
 pl.savefig('graph.png')
 pl.show()
